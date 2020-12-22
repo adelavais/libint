fec8a5e3b5d93908d7615f30d3aa38c1cb09e1b5
Adela Vais <adela.vais99@gmail.com>
2020-12-22 21:42:58 +0200

    declare the attributes globally change attribute from system to safe

--- source/libintl.d
+++ source/libintl.d
@@ -6,22 +6,22 @@
 module libintl;
 private import core.stdc.config;
 
-extern (C):
+extern (C) @safe nothrow @nogc:
 
-char* gettext (scope const char* __msgid) @system nothrow @nogc;
-char* dgettext (scope const char* __domainname, scope const char* __msgid) @system nothrow @nogc;
-char* dcgettext (scope const char* __domainname, scope const char* __msgid, int __category) @system nothrow @nogc;
-char* ngettext (scope const char* __msgid1, scope const char* __msgid2, c_long __n) @system nothrow @nogc;
+char* gettext (scope const char* __msgid);
+char* dgettext (scope const char* __domainname, scope const char* __msgid);
+char* dcgettext (scope const char* __domainname, scope const char* __msgid, int __category);
+char* ngettext (scope const char* __msgid1, scope const char* __msgid2, c_long __n);
 char* dngettext (scope const char* __domainname, scope const char* __msgid1, scope const char* __msgid2,
-                 c_long __n) @system nothrow @nogc;
+                 c_long __n);
 char* dcngettext (scope const char* __domainname, scope const char* __msgid1, scope const char* __msgid2,
-                  c_long __n, int __category) @system nothrow @nogc;
-char* textdomain (scope const char* __domainname) @system nothrow @nogc;
-char* bindtextdomain (scope const char* __domainname, scope const char* __dirname) @system nothrow @nogc;
-char* bind_textdomain_codeset (scope const char* __domainname, scope const char* __codeset) @system nothrow @nogc;
+                  c_long __n, int __category);
+char* textdomain (scope const char* __domainname);
+char* bindtextdomain (scope const char* __domainname, scope const char* __dirname);
+char* bind_textdomain_codeset (scope const char* __domainname, scope const char* __codeset);
 
 version (Darwin)
 {
-    char* __dgettext (scope const char* __domainname, scope const char* __msgid) @system nothrow @nogc;
-    char* __dcgettext (scope const char* __domainname, scope const char* __msgid, int __category) @system nothrow @nogc;
+    char* __dgettext (scope const char* __domainname, scope const char* __msgid);
+    char* __dcgettext (scope const char* __domainname, scope const char* __msgid, int __category);
 }
